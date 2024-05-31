5bbd705c86fae75075e8e68f0a17205bdf1315a1
Nata Nael <natanael280198@gmail.com>
2024-05-31 14:04:46 +0700

    feat: added error handler

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ const endpoints = {
   fetchPhotos: `https://weddingjs.avltree9798-uk.workers.dev/photos`,
 };
 
+const META_TAG_PLACEHOLDER = /__PLACEHOLDER_FOR_DYNAMIC_META_TAG__/;
+
 // Middleware to alter the meta tag and title in the HTML file
 app.use(async (req, res, next) => {
   if (req.path === "/" || req.path.endsWith(".html")) {
-    console.log(req.query.userId);
     fs.readFile(htmlFilePath, "utf8", async (err, data) => {
+      if (!req.query.userId || req.query.userId === "") {
+        let html = data.replace(META_TAG_PLACEHOLDER, "");
+        res.send(html);
+        return;
+      }
+
       if (err) {
         console.error("Error reading HTML file:", err);
         return res.status(500).send("Internal Server Error");
@@ -26,7 +33,7 @@ app.use(async (req, res, next) => {
       const resp = await fetch(
         "https://weddingjs.avltree9798-uk.workers.dev/content/6cd3e8ae-3d78-4ed1-b81b-a4b99bf29a72"
       );
-      const guestResp = await fetch(
+      const userResp = await fetch(
         `${endpoints.fetchUsers}/${req.query.userId}`,
         {
           method: "GET",
@@ -40,22 +47,26 @@ app.use(async (req, res, next) => {
       );
 
       const respData = await resp.json();
-      const guestData = await guestResp.json();
+      const guestData = await userResp.json();
       const photoData = await photoResp.json();
 
       // Modify the meta tag and title in the HTML content
 
       let modifiedHtml = data.replace(
-        /__PLACEHOLDER_FOR_DYNAMIC_META_TAG__/,
+        META_TAG_PLACEHOLDER,
         `<meta name="title" content="The wedding of Anthony & Leonie - Inviteyou" />
-        <meta name="description" content="Digital invitation for the wedding of Anthony & Leonie Madeleine for ${guestData.name}" />
-        <meta property="og:image" content=${photoData.main_photo} data-rh="true">
+        <meta name="description" content="Digital invitation for the wedding of Anthony & Leonie Madeleine for ${
+          guestData?.name || "you"
+        }" />
+        <meta property="og:image" content=${
+          photoData?.main_photo
+        } data-rh="true">
         `
       );
 
       modifiedHtml = modifiedHtml.replace(
         /<title>[^<]*<\/title>/,
-        `<title>The wedding of ${respData.groom_first_name} and ${respData.bride_first_name} - Ivitation</title>`
+        `<title>The wedding of ${respData?.groom_first_name} and ${respData?.bride_first_name} - Ivitation</title>`
       );
 
       res.send(modifiedHtml);
